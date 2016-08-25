54a3a594181d8f69100738d8bad2f38e3a0488c4
Usuario <Usuario@DESKTOP-RUQICM2>
2016-08-25 15:04:15 -0500

    variables de Menu

--- Tarea/src/tarea/Tarea.java
+++ Tarea/src/tarea/Tarea.java
@@ -5,6 +5,8 @@
  */
 package tarea;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Usuario
@@ -16,10 +18,17 @@ public class Tarea {
      */
     public static void main(String[] args) {
         // TODO code application logic her
+        Scanner ingreso=new Scanner(System.in);
+        int galones, opciones;
+        double diesel=1.50, gasolina=3.00;
+        double iva=12;
         System.out.println("Gasolinera");
         System.out.println("Bienvenidos a PetroEcuador");
         System.out.println("1)Gasolina $3.00 \n2)Diesel   $1.50");
+        opciones=ingreso.nextInt();
         System.out.println("ingrese la cantidad en Galones");
+        galones=ingreso.nextInt();
+       
     }
     
 }
